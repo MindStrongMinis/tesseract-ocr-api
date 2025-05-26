96efd98140a0887a395614d01c56e9f7883c623e
MindStrongMinis <mindstrongminis@gmail.com>
2025-05-26 23:53:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,3 +26,5 @@ def ocr():
         return jsonify({'text': text}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
